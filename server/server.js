8c4429446cd65c2f8069f2eca371a35c327937b2
Steven Martins <Steven@MacBook-Pro.local>
2020-06-02 16:39:28 -0700

    adding server folder to repo

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+//I belive these are built in
+const fs = require("fs");
+const app = express();
