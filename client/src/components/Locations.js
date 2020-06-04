ee5b07b625cc4b7c19da7006895472b1f8c2b12f
Nusorn <nkrachangtoy@gmail.com>
2020-06-04 16:56:16 -0700

    Add some style to location component

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -25,45 +25,31 @@ class Locations extends Component {
         <ul className="warehouse-list">
           <li className="warehouse-list__item">
             <div className="warehouse-list__warehose-wrapper">
-              <span className="warehouse-text--name">
-                {this.state.warehouseData.name}
-              </span>
-              <p className="warehouse-list__text--address">
-                {this.state.warehouseData.location}
-              </p>
+              <span className="warehouse-text--name">Warehouse Number 1</span>
+              <p className="warehouse-list__text--address">Location...</p>
             </div>
           </li>
           <li className="warehouse-list__item">
             <div className="warehouse-list__contact-wrapper">
-              <p className="warehouse-list__text--contact-name">
-                {this.state.warehouseData.contactName}
-              </p>
+              <p className="warehouse-list__text--contact-name">First Last</p>
               <p className="warehouse-list__text--contact-position">
-                {this.state.warehouseData.position}
+                Position...
               </p>
             </div>
           </li>
           <li className="warehouse-list__item">
             <div className="warehouse-list__contact-info-wrapper">
-              <p className="warehouse-list__text--number">
-                {this.state.warehouseData.phoneNumber}
-              </p>
-              <p className="warehouse-list__text--email">
-                {this.state.warehouseData.email}
-              </p>
+              <p className="warehouse-list__text--number">111-111-111</p>
+              <p className="warehouse-list__text--email">something@email.com</p>
             </div>
           </li>
           <li className="warehouse-list__item">
             <div className="warehouse-list__categories-wrapper">
-              <p className="warehouse-list__text--categories">
-                {this.state.warehouseData.categories}
-              </p>
+              <p className="warehouse-list__text--categories">Categories...</p>
+              <button className="warehouse-list__btn">
+                <img src={ArrowRight} alt="Arrow Right" />
+              </button>
             </div>
-          </li>
-          <li className="warehouse-list__item">
-            <button className="warehouse-list__btn">
-              <img src={ArrowRight} alt="Arrow Right" />
-            </button>
           </li>
         </ul>
       </div>
