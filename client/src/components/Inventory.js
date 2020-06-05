20c667e5542aa5c2b056cb2f5f87726ed58eafce
Steven Martins <Steven@MacBook-Pro.local>
2020-06-04 22:19:52 -0700

    Made some changes to warehouse.json, fed api to inventory, and did some styling

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
-
 import searchIcon from "../assets/Icons/SVG/Icon-search.svg";
 import kebabIcon from "../assets/Icons/SVG/Icon-kebab-default.svg";
+import axios from "axios";
+// import { Link } from "react-router-dom";
 
 export default class Inventory extends Component {
+  state = {
+    inventoryList: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/inventory").then((response) => {
+      this.setState({ inventoryList: response.data, loading: false });
+    });
+  }
+
   render() {
-    return (
+    return this.state.loading && this.state.instock !== [] ? (
+      <h1>Loading..</h1>
+    ) : (
       <div className="inventory">
         <div className="inventory__top-flex">
           <h1 className="inventory__title">Inventory</h1>
@@ -20,6 +34,7 @@ export default class Inventory extends Component {
               className="inventory__search-bar"
               name="search"
               placeholder="Search"
+              autoComplete="off"
             />
           </form>
         </div>
@@ -34,30 +49,39 @@ export default class Inventory extends Component {
             </label>
           </div>
         </div>
-
-        <div className="inventory__container">
-          <div className="inventory__flex">
-            <label className="inventory__product-margin inventory__mobile">
-              ITEM
-            </label>
-            <div className="inventory__tablet-product">
-              <p className="inventory__shrink">description...</p>
+        {this.state.inventoryList.map((inv) => {
+          return (
+            <div className="inventory__container">
+              <div className="inventory__flex">
+                <label className="inventory__product-margin inventory__mobile">
+                  ITEM
+                </label>
+                <div className="inventory__tablet-product">
+                  {/* <Link
+                    to={`/product/${inv.productId}`}
+                    className="inventory__product"
+                  > */}
+                  <p className="inventory__product-name"> {inv.productName} </p>
+                  {/* </Link> */}
+                  <p className="inventory__shrink">{inv.description}</p>
+                </div>
+                <div className="inventory__tablet-stats">
+                  <label className="inventory__mobile">LAST ORDERED</label>
+                  <p className="inventory__tablet-right">05/24/2018</p>
+                  <label className="inventory__mobile">LOCATION</label>
+                  <p className="inventory__tablet-right">Toronto, CAN</p>
+                  <label className="inventory__mobile">QUANTITY</label>
+                  <p className="inventory__tablet-right">12,000</p>
+                  <label className="inventory__mobile">STATUS</label>
+                  <p className="inventory__tablet-right kebab">{inv.inStock}</p>
+                </div>
+              </div>
+              <div className="inventory__options">
+                <img src={kebabIcon} alt="options" />
+              </div>
             </div>
-            <div className="inventory__tablet-stats">
-              <label className="inventory__mobile">LAST ORDERED</label>
-              <p className="inventory__tablet-right">Ordered date...</p>
-              <label className="inventory__mobile">LOCATION</label>
-              <p className="inventory__tablet-right">Location...</p>
-              <label className="inventory__mobile">QUANTITY</label>
-              <p className="inventory__tablet-right">quantity</p>
-              <label className="inventory__mobile">STATUS</label>
-              <p className="inventory__tablet-right kebab">In Stock</p>
-            </div>
-          </div>
-          <div className="inventory__options">
-            <img src={kebabIcon} alt="options" />
-          </div>
-        </div>
+          );
+        })}
       </div>
     );
   }
