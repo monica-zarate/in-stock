1509b057efd5741bc262d546089cd6bc2f4f4a14
Nusorn <nkrachangtoy@gmail.com>
2020-06-04 19:30:10 -0700

    Finish styling for Location Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./style/main.css";
-
-import Locations from "./components/Locations";
-import Inventory from "./components/Inventory";
 import Header from "./components/Header";
-
+import Inventory from "./components/Inventory";
+import Locations from "./components/Locations";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Inventory />
-      <Locations />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/inventory" component={Inventory}></Route>
+          <Route path="/locations" component={Locations}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
 export default App;
--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -12,21 +12,26 @@ class Locations extends Component {
     return (
       <div className="location">
         <div className="location__top-wrapper">
-          <span className="location__title">Locations</span>
+          <h1 className="location__title">Locations</h1>
           <form className="location__form">
             <input
               type="search"
               name="search"
               placeholder="Search"
-              className="location__search-input"
+              className="location__search-box"
             />
           </form>
         </div>
         <ul className="warehouse-list">
           <li className="warehouse-list__item">
-            <div className="warehouse-list__warehose-wrapper">
-              <span className="warehouse-text--name">Warehouse Number 1</span>
+            <div className="warehouse-list__warehouse-wrapper">
+              <span className="warehouse-list__text--name">
+                Warehouse Number 1
+              </span>
               <p className="warehouse-list__text--address">Location...</p>
+              <button className="warehouse-list__btn">
+                <img src={ArrowRight} alt="Arrow Right" />
+              </button>
             </div>
           </li>
           <li className="warehouse-list__item">
@@ -39,16 +44,15 @@ class Locations extends Component {
           </li>
           <li className="warehouse-list__item">
             <div className="warehouse-list__contact-info-wrapper">
-              <p className="warehouse-list__text--number">111-111-111</p>
+              <p className="warehouse-list__text--number">+1 111 111 111</p>
               <p className="warehouse-list__text--email">something@email.com</p>
             </div>
           </li>
           <li className="warehouse-list__item">
             <div className="warehouse-list__categories-wrapper">
-              <p className="warehouse-list__text--categories">Categories...</p>
-              <button className="warehouse-list__btn">
-                <img src={ArrowRight} alt="Arrow Right" />
-              </button>
+              <p className="warehouse-list__text--categories">
+                Industrial, Automotive, Heavy, Mechanical, Engineering
+              </p>
             </div>
           </li>
         </ul>
