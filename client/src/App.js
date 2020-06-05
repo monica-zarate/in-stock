ced3a9a501e8f6e1bb53be9b5252d2174991fa48
Monica Zhazil <monicazhazil@MacBook-Pro.hitronhub.home>
2020-06-04 18:40:03 -0700

    Enabled Links on Header Component. Added Routes to App.js. Updated html tags in Locations Components.

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,17 @@ import Locations from "./components/Locations";
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <Locations />
+=======
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/inventory" component={Inventory}></Route>
+          <Route path="/locations" component={Locations}></Route>
+        </Switch>
+      </Router>
+>>>>>>> Stashed changes
     </div>
   );
 }
--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -5,7 +5,7 @@ class Locations extends Component {
     return (
       <div className="container location">
         <div className="location__top-wrapper">
-          <span className="location__title">Locations</span>
+          <h1 className="location__title">Locations</h1>
           <form className="location__form">
             <input
               type="search"
@@ -14,6 +14,39 @@ class Locations extends Component {
             />
           </form>
         </div>
+<<<<<<< Updated upstream
+=======
+        <ul className="warehouse-list">
+          <li className="warehouse-list__item">
+            <div className="warehouse-list__warehose-wrapper">
+              <h3 className="warehouse-text--name">Warehouse Number 1</h3>
+              <p className="warehouse-list__text--address">Location...</p>
+            </div>
+          </li>
+          <li className="warehouse-list__item">
+            <div className="warehouse-list__contact-wrapper">
+              <p className="warehouse-list__text--contact-name">First Last</p>
+              <p className="warehouse-list__text--contact-position">
+                Position...
+              </p>
+            </div>
+          </li>
+          <li className="warehouse-list__item">
+            <div className="warehouse-list__contact-info-wrapper">
+              <p className="warehouse-list__text--number">111-111-111</p>
+              <p className="warehouse-list__text--email">something@email.com</p>
+            </div>
+          </li>
+          <li className="warehouse-list__item">
+            <div className="warehouse-list__categories-wrapper">
+              <p className="warehouse-list__text--categories">Categories...</p>
+              <button className="warehouse-list__btn">
+                <img src={ArrowRight} alt="Arrow Right" />
+              </button>
+            </div>
+          </li>
+        </ul>
+>>>>>>> Stashed changes
       </div>
     );
   }
