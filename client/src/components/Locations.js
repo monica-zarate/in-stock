afee182db5cf4300130675f50d7939a22e2b746e
Nusorn <nkrachangtoy@gmail.com>
2020-06-06 00:39:47 -0700

    Connect api with locations component

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import ArrowRight from "../assets/Icons/SVG/Icon-arrow-right.svg";
 
 class Locations extends Component {
+  state = {
+    warehouseList: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/locations")
+      .then((response) => {
+        this.setState({
+          warehouseList: response.data,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
   render() {
     return (
       <div className="container location">
@@ -16,40 +32,52 @@ class Locations extends Component {
             />
           </form>
         </div>
-        <ul className="warehouse-list">
-          <li className="warehouse-list__item">
-            <div className="warehouse-list__warehouse-wrapper">
-              <span className="warehouse-list__text--name">
-                Warehouse Number 1
-              </span>
-              <p className="warehouse-list__text--address">Location...</p>
-              <button className="warehouse-list__btn">
-                <img src={ArrowRight} alt="Arrow Right" />
-              </button>
-            </div>
-          </li>
-          <li className="warehouse-list__item">
-            <div className="warehouse-list__contact-wrapper">
-              <p className="warehouse-list__text--contact-name">First Last</p>
-              <p className="warehouse-list__text--contact-position">
-                Position...
-              </p>
-            </div>
-          </li>
-          <li className="warehouse-list__item">
-            <div className="warehouse-list__contact-info-wrapper">
-              <p className="warehouse-list__text--number">+1 111 111 111</p>
-              <p className="warehouse-list__text--email">something@email.com</p>
-            </div>
-          </li>
-          <li className="warehouse-list__item">
-            <div className="warehouse-list__categories-wrapper">
-              <p className="warehouse-list__text--categories">
-                Industrial, Automotive, Heavy, Mechanical, Engineering
-              </p>
-            </div>
-          </li>
-        </ul>
+        {this.state.warehouseList.map((warehouse) => {
+          return (
+            <ul className="warehouse-list">
+              <li className="warehouse-list__item">
+                <div className="warehouse-list__warehouse-wrapper">
+                  <span className="warehouse-list__text--name">
+                    {warehouse.name}
+                  </span>
+                  <p className="warehouse-list__text--address">
+                    {warehouse.address}
+                  </p>
+                  <button className="warehouse-list__btn">
+                    <img src={ArrowRight} alt="Arrow Right" />
+                  </button>
+                </div>
+              </li>
+              <li className="warehouse-list__item">
+                <div className="warehouse-list__contact-wrapper">
+                  <p className="warehouse-list__text--contact-name">
+                    {warehouse.contactName}
+                  </p>
+                  <p className="warehouse-list__text--contact-position">
+                    {warehouse.position}
+                  </p>
+                </div>
+              </li>
+              <li className="warehouse-list__item">
+                <div className="warehouse-list__contact-info-wrapper">
+                  <p className="warehouse-list__text--number">
+                    +1 {warehouse.phoneNumber}
+                  </p>
+                  <p className="warehouse-list__text--email">
+                    {warehouse.email}
+                  </p>
+                </div>
+              </li>
+              <li className="warehouse-list__item">
+                <div className="warehouse-list__categories-wrapper">
+                  <p className="warehouse-list__text--categories">
+                    {warehouse.categories}
+                  </p>
+                </div>
+              </li>
+            </ul>
+          );
+        })}
       </div>
     );
   }
