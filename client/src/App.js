633cb852a7efcec7fe31556ce237dc4b9f88e8f6
Steven Martins <Steven@MacBook-Pro.local>
2020-06-06 17:51:09 -0700

    added some routes to app.js and edited warehouse.json. Implemented changes to Inventory.js and trying to get products page to work

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Inventory from "./components/Inventory";
 import Locations from "./components/Locations";
 
 import "./style/main.css";
+import Products from "./components/Products";
 
 function App() {
   return (
@@ -22,6 +23,9 @@ function App() {
         <Switch>
           <Route path="/inventory" component={Inventory} exact />
           <Route path="/locations" component={Locations} exact />
+          <Route path="/product/:id" component={Products} exact />
+          <Redirect to="/inventory" from="/" exact />
+          <Redirect to="/inventory" from="/product" exact />
         </Switch>
       </Router>
     </div>
---
+++ client/src/components/Products.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Products extends Component {
+  state = {
+    product: [],
+  };
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    axios
+      .get("http://localhost:5000/product/:id")
+      .then((response) => {
+        const res = response.data.filter((item) => {
+          return item.productId === id;
+        });
+        this.setState({
+          product: res,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  render() {
+    return (
+      <div className="product">
+        {this.state.product.map((item) => {
+          return (
+            <div className="product__container" id={item.productId}>
+              <div className="product__header">
+                <h1 className="product__label-page">{item.productName}</h1>
+                <div className="product__instock-notifier">
+                  <p className="product__instock-notifier-text">
+                    {item.inStock}
+                  </p>
+                </div>
+              </div>
+              <hr className="product__hr"></hr>
+
+              <div className="product__description-details-container">
+                <div className="product__description-container">
+                  <h4 className="product__label-description">
+                    ITEM DESCRIPTION
+                  </h4>
+                  <p className="product__item-description">
+                    {item.description}
+                  </p>
+                </div>
+                <div className="product__details-container">
+                  <div className="product__info-details">
+                    <div className="product__label-value-container">
+                      <h4 className="product__label-last-ordered">
+                        LAST ORDERED
+                      </h4>
+                      <p className="product__last-ordered">
+                        {item.lastOrdered}
+                      </p>
+                    </div>
+                    <div className="product__label-value-container">
+                      <h4 className="product__label-location">LOCATION</h4>
+                      <p className="product__location">{item.location}</p>
+                    </div>
+                  </div>
+
+                  <h4 className="product__label-quantity">QUANTITY</h4>
+                  <p className="product__quantity">{item.quantity}</p>
+
+                  <h4 className="product__label-categories">CATEGORIES</h4>
+                  <p className="product__categories">{item.categories}</p>
+                </div>
+              </div>
+              <hr className="product__hr"></hr>
+              <div className="product__btn-container">
+                <button className="product__edit-btn">EDIT</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
